42132fc4bffdd32eab70f1b1a33c5a6ebfef2358
Falco <falcoauction@gmail.com>
2026-02-18 13:49:04 -0600

    Update run_all.py

--- src/run_all.py
+++ src/run_all.py
@@ -1,11 +1,17 @@
+# src/run_all.py
+
 print("RUN_ALL VERSION CHECK - 2026-02-18")
 
 from .bots.public_notices_bot import run as run_public_notices
+from .bots.foreclosure_tennessee_bot import run as run_foreclosure_tn
 from .bots.tax_pages_bot import run as run_tax_pages
+
 
 def main():
     run_public_notices()
+    run_foreclosure_tn()
     run_tax_pages()
+
 
 if __name__ == "__main__":
     main()
