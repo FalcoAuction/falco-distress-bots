a7b4f5f451f4b59676f46b0338d255c3a7c8859b
Falco <falcoauction@gmail.com>
2026-02-17 20:36:31 -0600

    Create run_all.py

---
+++ src/run_all.py
@@ -0,0 +1,9 @@
+from .bots.public_notices_bot import run as run_public_notices
+from .bots.tax_pages_bot import run as run_tax_pages
+
+def main():
+    run_public_notices()
+    run_tax_pages()
+
+if __name__ == "__main__":
+    main()
